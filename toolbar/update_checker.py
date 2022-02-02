dd7d7341935398998b08dbfb920cf339dc37d4a7
w1png <w1png@yandex.ru>
2022-02-03 01:34:56 +0300

    Delete unused accidental import

--- toolbar/update_checker.py
+++ toolbar/update_checker.py
@@ -1,14 +1,12 @@
 from os import popen
 import platform
 
-from psutil import LINUX
-
 LINUX_DISTRO = platform.freedesktop_os_release()["ID"]
 
 if LINUX_DISTRO == "arch":
     update_amount = popen("checkupdates | wc -l").read()
     print("No new updates!" if update_amount == 0 else ("New updates: " + str(update_amount)))
-    
+
 elif LINUX_DISTRO == "fedora":
     try:
         print("New updates: " + str(popen("dnf check-update").read().split("\n")[::-1][1:].index("")))
