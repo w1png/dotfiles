f7b7650d72613b83d203cb4e8fdd06109a7902d5
w1png <w1png@yandex.ru>
2022-02-03 01:33:09 +0300

    Update update-checker to support arch and fedora

--- toolbar/update_checker.py
+++ toolbar/update_checker.py
@@ -1,9 +1,20 @@
 from os import popen
-from typing import ValuesView
+import platform
 
-try:
-    print("New updates: " + str(popen("dnf check-update").read().split("\n")[::-1][1:].index("")))
-except ValueError:
-    print("No new updates!")
-except:
-    print("Error in update-checker.py!")
+from psutil import LINUX
+
+LINUX_DISTRO = platform.freedesktop_os_release()["ID"]
+
+if LINUX_DISTRO == "arch":
+    update_amount = popen("checkupdates | wc -l").read()
+    print("No new updates!" if update_amount == 0 else ("New updates: " + str(update_amount)))
+    
+elif LINUX_DISTRO == "fedora":
+    try:
+        print("New updates: " + str(popen("dnf check-update").read().split("\n")[::-1][1:].index("")))
+    except ValueError:
+        print("No new updates!")
+    except:
+        print("Error in update-checker.py!")
+else:
+    print("Unknown linux distribution :(")
